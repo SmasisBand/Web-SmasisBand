d5e4af6ce11b6976863fd4d666b7a3ca6f608359
SmasisBand <168838194+SmasisBand@users.noreply.github.com>
2024-05-03 22:42:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".menu")
+const icon_menu = document.querySelector(".icon")
+const menus = document.querySelector(".moff")
+
+
+
+icon_menu.addEventListener("click",()=>
+{
+    menu.classList.toggle("on")
+})
