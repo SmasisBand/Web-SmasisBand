837e4ea2b4afaa38cb17f426a879243d8df7faef
SmasisBand <168838194+SmasisBand@users.noreply.github.com>
2024-05-20 20:26:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const menu = document.querySelector(".menu")
 const icon_menu = document.querySelector(".icon")
 const menus = document.querySelector(".moff")
 
-
-
 icon_menu.addEventListener("click",()=>
 {
     menu.classList.toggle("on")
+})
+
+window.addEventListener("scroll",()=>
+{
+    const navigasi = document.querySelector(".navigasi")
+    navigasi.classList.toggle("onscroll",window.scrollY > 0)
 })
